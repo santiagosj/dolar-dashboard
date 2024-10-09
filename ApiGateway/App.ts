5fcf0c70a00de01386aa5702a2beb663e399b8a9
santiagosj <sanjs965@gmail.com>
2024-10-08 22:05:38 -0300

    first commit

---
+++ ApiGateway/App.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+// importar rutas aqui
+
+export default app;
---
+++ ApiGateway/index.ts
@@ -0,0 +1,7 @@
+import app from "./App"
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Api Gatetway is running on port ${PORT}`);
+})
